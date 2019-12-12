74c9bf976286607d0b44ab0c28ab3ba740c3c626
Martin Neuenhofen <martin.neuenhofen@truetzschler.de>
2019-12-12 08:51:51 +0100

    batch script for recreating test packages.

---
+++ create_script.bat
@@ -0,0 +1,68 @@
+cd Packages
+
+
+cd FinitediffPackage
+cd 1.0.0
+conan create .
+cd ..
+cd 1.1.0
+conan create .
+cd ..
+cd 1.2.0
+conan create .
+cd ..
+cd 1.3.0
+conan create .
+cd ..
+cd 2.0.0
+conan create .
+cd ..
+cd ..
+
+
+cd RootsolversPackage
+cd 1.0.0
+conan create .
+cd ..
+cd 2.1.0
+conan create .
+cd ..
+cd 2.1.0
+conan create .
+cd ..
+cd 2.5.0
+conan create .
+cd ..
+cd 2.6.0
+conan create .
+cd ..
+cd 3.0.0
+conan create .
+cd ..
+cd ..
+
+
+cd MinimizersPackage
+cd 1.0.0
+conan create .
+cd ..
+cd 1.1.0
+conan create .
+cd ..
+cd 1.2.0
+conan create .
+cd ..
+cd 2.0.0
+conan create .
+cd ..
+cd 2.1.0
+conan create .
+cd ..
+cd 2.2.0
+conan create .
+cd ..
+cd 3.0.0
+conan create .
+cd ..
+cd ..
+
